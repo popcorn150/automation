057d19b22d51c1539c1bccc2337af10f049ad6ba
popcorn150 <hungrymanchannel@gmail.com>
2023-10-17 17:09:53 +0100

    Basic - Relationship[Married]

--- test/matchingday.spec.ts
+++ test/matchingday.spec.ts
@@ -192,4 +192,36 @@ test.describe.parallel.only('Building the profile', () => {
     await page.getByRole('button', { name: 'Save' }).click()
     await page.waitForTimeout(10000)
   })
+
+  test('Basic - Relationship[Married]', async ({ page }) => {
+    await page.goto('https://dotunpeters.matchingday.com/')
+    await page.getByRole('button').click()
+    await page.getByRole('link', { name: 'Sign in' }).click()
+    await page
+      .getByRole('button', { name: 'Google Icon Continue with Google' })
+      .click()
+    await page.getByLabel('Email or phone').click()
+    await page.getByLabel('Email or phone').fill('david.damian@10hourlabs.com')
+    await page.getByRole('button', { name: 'Next' }).click()
+    await page.getByLabel('Enter your password').click()
+    await page.getByLabel('Enter your password').fill('D3YS6%gy')
+    await page.getByRole('button', { name: 'Next' }).click()
+    await page.waitForTimeout(1000)
+
+    // Relationship
+    await page.getByRole('button', { name: 'Edit' }).click()
+
+    // Relationship status
+    await page.locator('#mantine-msepabtob').selectOption('Married')
+
+    // Years togehter
+    await page.locator('#mantine-9xu7e95ik').selectOption('1 to 5 years')
+
+    // Type of couple
+    await page.locator('#mantine-1p9jngkbk').selectOption('Heterosexual')
+
+    //saving...
+    await page.getByRole('button', { name: 'Save' }).click()
+    await page.waitForTimeout(10000)
+  })
 })
